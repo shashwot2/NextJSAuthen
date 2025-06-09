7de85e8266b3e262e0703440708c949a9df3fef1
Shashwot KC <49743516+shashwot2@users.noreply.github.com>
2025-06-09 14:27:49 -0400

    Fix logout with same set cookie attributes and some cleanup

--- src/app/api/logout/route.ts
+++ src/app/api/logout/route.ts
@@ -1,9 +1,10 @@
 export const POST = async (request: Request) => {
+    console.log("received logout req")
     return new Response(JSON.stringify({ message: 'Logged out successfully' }), {
         status: 200,
         headers: {
             'Content-Type': 'application/json',
-            'Set-Cookie': 'token=; Max-Age=0'
+            'Set-Cookie': 'token=; HttpOnly; SameSite=Strict; Max-Age=0; Path=/'
         }
     })
 }
